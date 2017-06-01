8ddf11f234a74ae6d7fc5400b1363836ea1fdc87
Greg Sargent <gksargent@byui.edu>
2017-05-31 22:08:24 -0500

    Initial upload of gsargent.info files

---
+++ gsargent.info/modules/footer.php
@@ -0,0 +1,17 @@
+  <footer>
+
+    <div id="footer-left-column">
+      <p>&copy; Greg Sargent &nbsp; | &nbsp; Proudly coded from scratch.</p>
+    </div>
+
+    <div id="footer-right-column">
+      <ul>
+        <li><a href="https://www.linkedin.com/in/gksargent/" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a></li>
+        <li><a href="https://dribbble.com/gksargent" target="_blank"><i class="fa fa-dribbble fa-lg" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.behance.net/gksargent44bc" target="_blank"><i class="fa fa-behance fa-lg" aria-hidden="true"></i></a></li>
+        <li><a href="https://codepen.io/gsargent/" target="_blank"><i class="fa fa-codepen fa-lg" aria-hidden="true"></i></a></li>
+        <li><a href="https://github.com/gksargent" target="_blank"><i class="fa fa-github fa-lg" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+
+  </footer>
