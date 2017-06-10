7b91d56ad65cc637d3f5a6d805e0a8841701115a
Greg Sargent <gksargent@byui.edu>
2017-06-10 11:56:35 -0500

    cit301b subdomain files

--- gsargent.info/college/cit301b/zengarden.php
+++ gsargent.info/college/cit301b/zengarden.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <title>BYU-I Web Design &amp; Development | Greg Sargent - UX/UI Designer</title>
+    <title>ZenGarden Group Project | Greg Sargent - UX/UI Designer</title>
     <meta name="description" content="Greg is a UX/UI Designer in the north Dallas area. Specializing in minimal modern design, Greg is currently working in the FinTech industry designing personal financial management tools at Alkami.">
     <?php // This php pulls in head.php to create the meta and links
     include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
@@ -13,16 +13,14 @@
   include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
 
   <div id="banner">
-    <h1>BYU-I Web Design &amp; Development</h1>
-    <p>I'm currently a junior at Brigham Young University Idaho working on a BS in web design and development with an emphasis on design. Here I'll include links to work I've completed for classes and little things that I'm learning about.</p>
+    <h1>ZenGarden Group Project</h1>
+    <p>A group project to plan, design and develop a ZenGarden project using modularized scss partials.</p>
   </div>
 
   <main>
-    <h2>COMM497R - Special Topics Practicum</h2>
-    <a href="/college/comm497r/site-walkthrough.php" target="_self">Class site walkthrough</a>
-    <hr>
-    <h2>CIT301B - Advanced CSS</h2>
-    <a href="/college/cit301b/zengarden.php" target="_self">ZenGarden group project</a>
+
+    <p>Check out the <a href="https://github.com/gksargent/cit301b-zengarden" target="_blank">GitHub Project</a>!</p>
+
   </main>
 
 
